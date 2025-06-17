faf0114b0e5469f508974ea4cbb1c1a10ddae909
Carlos <carlos.silva1694@etec.sp.gov.br>
2025-06-17 15:22:52 -0300

    Melhoria

--- Calculadora/calculadora.c
+++ Calculadora/calculadora.c
@@ -5,29 +5,37 @@
 void main(){
 	float x, y, resultado;
 	int escolha;
+	char escolha2;
 	printf("Digite um número.\n");
 	scanf("%f",&x);
 	printf("Digite outro número.\n");
 	scanf("%f",&y);
-	printf("Escolha uma forma de cálculo.\n1-Soma\n2-Subtração\n3-Multiplicação\n4-Divisão\nQ-Sair");
+	printf("Escolha uma forma de cálculo.\nA-Soma\nS-Subtração\nM-Multiplicação\nD-Divisão\nQ-Sair\n");
 	scanf("%d",&escolha);
-	
-	switch (escolha){
-		case 1:
+	scanf("%c",&escolha2);
+
+	switch (escolha, escolha2){
+		case 'A':
 			resultado = soma(x,y);
 			printf("Você escolheu Soma.\nO resultado da soma dos seus números é:%f\n",resultado);
 			break;
-		case 2:
+		case 'S':
 			resultado = subtracao(x,y);
 			printf("Você escolheu Subtração.\nO resultado da subtração dos seus números é:%f\n",resultado);
 			break;
-		case 3:
+		case 'M':
 			resultado = multiplicacao(x,y);
 			printf("Você escolheu Multiplicação.\nO resultado da multiplicação é:%f\n",resultado);
 			break;
-		case 4:
+		case 'D':
 			resultado = divisao(x,y);
 			printf("Você escolheu Divisão.\nO resultado da divisão é:%f\n",resultado);
+			break;
+		case 'Q':
+			printf("Você escolheu Sair.\n");
+			break;
+		default:
+			printf("Opção invalida.\n");
 			break;
 	}
 }
